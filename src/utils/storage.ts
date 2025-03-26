39fb8762f97e3c976939434a6016bb2ec62ac40a
Jpleseux <joaopleseux@gmail.com>
2025-03-25 23:16:17 -0400

    feat: Banco finalizado e envs

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,25 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 import { ShoppingList, ShoppingItem } from '../types';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const supabaseUrl = 'https://lsutyjmahtfxqmjfqkpp.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzdXR5am1haHRmeHFtamZxa3BwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5NTM0NzgsImV4cCI6MjA1ODUyOTQ3OH0.ezQeuMsSEqvvERFEWMcnokmuWb5A1xND5OmPBNlB3Bk';
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseAnonKey = process.env.SUPABASE_KEY as string;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const TABLE_NAME = 'shopping_list';
-
-// Ajuste os nomes das propriedades para letras minúsculas
-const serializeItem = (item: ShoppingItem): ShoppingItem => ({
-  ...item,
-  createdAt: item.createdAt instanceof Date ? item.createdAt.toISOString() : item.createdAt,
-  updatedAt: item.updatedAt instanceof Date ? item.updatedAt.toISOString() : item.updatedAt,
-});
-
-const deserializeItem = (item: any): ShoppingItem => ({
-  ...item,
-  createdAt: new Date(item.createdat),
-  updatedAt: new Date(item.updatedat),
-});
 
 export const loadShoppingList = async (): Promise<ShoppingList> => {
   try {
@@ -38,7 +27,7 @@ export const loadShoppingList = async (): Promise<ShoppingList> => {
         quantity: item.quantity,
         completed: item.completed,
         createdAt: new Date(item.createdat),
-        updatedAt: new Date(item.updatedat),  // Mantém updatedAt do Supabase
+        updatedAt: new Date(item.updatedat),
         price: item.price,
       })) : [],
     };
